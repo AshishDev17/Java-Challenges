1ecbbcf83faccf39cc0013802ad62d37eae53934
AshishDev17 <aashmalav@gmail.com>
2019-02-23 20:25:07 -0500

    Alphabet Soup Challenge

--- AlphabetSoup.java
+++ AlphabetSoup.java
@@ -1,13 +1,20 @@
+import java.util.Arrays;
+
 //This challenge requires you to alphabetically sort the characters in a string.
 
 public class AlphabetSoup {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println(AlphabetSoup.sorted("1co80der24c22b!yte45"));
+		System.out.println(AlphabetSoup.sorted("bdgthea"));
+		System.out.println(AlphabetSoup.sorted("45626189"));
+		System.out.println(AlphabetSoup.sorted("abcdefgh"));
 	}
 	
 	public static String sorted(String str) {
+		char[] chrArray = str.toCharArray();
+		Arrays.sort(chrArray);
 		
+		return new String(chrArray);
 	}
 }
