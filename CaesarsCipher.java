e74ca976486c09d11ab86329530f962954055a12
AshishDev17 <aashmalav@gmail.com>
2019-02-24 13:07:32 -0500

    Caesars Cipher challenge refactor

--- CaesarsCipher.java
+++ CaesarsCipher.java
@@ -1,30 +1,49 @@
+enum Type { 
+    lc,
+    up;
+} 
+
+enum Change {
+	encrypt,
+	decrypt;
+}
 
 public class CaesarsCipher {
 	public static void main(String[] args) {
 		//CaesarsCipher.printChar("char");
-		System.out.println(CaesarsCipher.rotLowerCase("char", 1));
-		System.out.println(CaesarsCipher.rotUpperCase("CHAR", 1));
+		System.out.println(CaesarsCipher.changeLetters("char", 1, Type.lc, Change.encrypt));
+		System.out.println(CaesarsCipher.changeLetters("CHAR", 1, Type.up, Change.encrypt));
+		System.out.println(CaesarsCipher.changeLetters("dibs", 1, Type.lc, Change.decrypt));
+		System.out.println(CaesarsCipher.changeLetters("DIBS", 1, Type.up, Change.decrypt));
 	}
 	
-	//all letters in the string are lower case
-	public static String rotLowerCase(String str, int shift) {
+	public static StringBuffer changeLetters(String str, int shift, Type type, Change change) {
 		int length = str.length();
-		StringBuilder newStr = new StringBuilder();
-		int unicodeOfA = "a".codePointAt(0);
+		StringBuffer newStr = new StringBuffer();
+		int unicodeOfA = 0;
+		
+		if(type == Type.lc) {
+			unicodeOfA = "a".codePointAt(0);
+		} else if(type == Type.up) {
+			unicodeOfA = "A".codePointAt(0);
+		}
 		
 		for(int i = 0; i < length; i++) {
 			int unicodeOfI = str.codePointAt(i);
+			if(change == Change.encrypt) {
+				unicodeOfI = unicodeOfA + ((unicodeOfI - unicodeOfA + shift) % 26);
+			} else if(change == Change.decrypt) {
+				unicodeOfI = unicodeOfA + ((unicodeOfI - unicodeOfA + (26 - shift)) % 26);
+			}
 			
-			unicodeOfI = unicodeOfA + ((unicodeOfI - unicodeOfA + shift) % 26);
 			newStr.append((char)unicodeOfI);
 		}
 		
-		return newStr.toString();		
+		return newStr;		
 	}
 	
-	public static String rotUpperCase(String str, int shift) {
-		return CaesarsCipher.rotLowerCase(str.toLowerCase(), shift).toUpperCase();
-	}
+	
+	
 	
 //	public static void printChar(String str) {
 //		char[] charArr = str.toCharArray();
